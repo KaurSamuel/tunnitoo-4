9b58d3a3e5e75feea9d3c2dbe75236aecea37e97
KaurSamuel <33308949+KaurSamuel@users.noreply.github.com>
2018-02-22 09:52:00 +0200

    done

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string data = System.IO.File.ReadAllText("../../ tekst.txt");
-            Console.WriteLine(data);
+            string tekst = System.IO.File.ReadAllText("../../tekst.txt");
+
+            int mittusona = (from word in tekst.Substring(0) select word).Count();
+            Console.WriteLine("tekstifailis on "+ mittusona + " sõna");
+
+            int mittu_tähte = (from char1 in tekst.ToList().Distinct()where tekst !=" " select char1).Count();
+            Console.WriteLine("tekstis on "+mittu_tähte+" erinevat tähemärki");
+
+            char[] kõiktähed = (from char1 in tekst.ToList().Distinct() where tekst != " " where tekst != "" select char1).ToArray();
+
+            var sort = from s in kõiktähed orderby s select s;
+
+            foreach (var item in sort)
+            {
+                int mittutähte = (from char1 in tekst.ToList() where char1 == item select char1).Count();
+                Console.WriteLine(item+ " = "+ mittutähte);
+            }
+            string[]words = tekst.Split(new char[] { ' '},
+                StringSplitOptions.RemoveEmptyEntries);
+            var word_query =
+                (from string word in words
+                orderby word
+                select word).Distinct();
+            string[] words1 = word_query.ToArray();
+            foreach (var item in words1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
     }
 }
